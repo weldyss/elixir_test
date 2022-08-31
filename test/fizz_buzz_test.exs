b666c8ec341c7e692455f39a012d8c7f759ada5c
Weldys Santos <weldys@icloud.com>
2022-08-31 11:19:35 -0300

    first commit

---
+++ test/fizz_buzz_test.exs
@@ -0,0 +1,37 @@
+defmodule FizzBuzzTest do
+  use ExUnit.Case
+
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response =
+        {:ok,
+         [
+           1,
+           2,
+           :fizz,
+           4,
+           :buzz,
+           :fizz,
+           :fizzbuzz,
+           :fizz,
+           :fizz,
+           26,
+           32,
+           :fizzbuzz,
+           56,
+           :fizz,
+           578,
+           34123,
+           :fizz
+         ]}
+
+      assert FizzBuzz.build("numbers.txt") == expected_response
+    end
+
+    test "when a invalid file is provided, returns an error" do
+      expected_response = {:error, "Error reading the file: enoent"}
+
+      assert FizzBuzz.build("wrong_file.txt") == expected_response
+    end
+  end
+end
